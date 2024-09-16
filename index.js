a8bf8b1bcc411e076c5b55097428806950aad2f5
dl munkhod <dlmunkhod@dls-MacBook-Air.local>
2024-09-16 22:16:14 +0800

    new

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hello Typescript");
+var num = 20;
+var str = "hello";
+var isBool = true;
+// typescript gedeg n turul buguud JS ruu durmiin daguu turluudiig n zaaj ugnu. Tegeed typescriptee javascript ruu hurvuulne. Ingesneer aldaag uridchilan typescripteer turul butiin durmiin daguu aldaagui bichne.
+// typescript => javascript ruu hurvuulne
+// --oOo--
+// typescript-g compider ashiglaj js ruu hurvuulne
+// node js bwr zuvhun js-g ajilluuldag
+console.log("Num value", num);
+console.log("Str value", str);
+console.log("Boolean value", isBool);
+// javascrip-g jaahan urgutguud typescript (MS gargaj irsen)-g gargaj irj aldaagui bichih bolomjiig olgoson.
+function add(a, b) {
+    return a + b;
+}
+var y = add(100, 100);
+console.log("ADD", y);
+// zaaval toon utga butsaana gedriig bichne.
+var add2 = function (a, b) {
+    return a + b;
+};
+var myPrint = function (text) {
+    console.log("Hello", text);
+};
+// print n ugsun toog console.log ruu heveldeg function. herev VOID geed bichvel utga butsaagui functs.
+var s = add2(200, 500);
+myPrint("Welcome");
+// type imperence n anhnii utgaar n oilgono
