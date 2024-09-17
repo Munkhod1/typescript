04166e4e1eb939170f7b61aec4fa783bdc468151
Munkhod1 <munkhoddl@gmail.com>
2024-09-17 11:19:47 +0800

    0917

---
+++ build/index.js
@@ -0,0 +1,31 @@
+"use strict";
+console.log("hello Typescript");
+const num = 20;
+const str = "hello";
+const isBool = true;
+// typescript gedeg n turul buguud JS ruu durmiin daguu turluudiig n zaaj ugnu. Tegeed typescriptee javascript ruu hurvuulne. Ingesneer aldaag uridchilan typescripteer turul butiin durmiin daguu aldaagui bichne.
+// typescript => javascript ruu hurvuulne
+// --oOo--
+// typescript-g compider ashiglaj js ruu hurvuulne
+// node js bwr zuvhun js-g ajilluuldag
+console.log("Num value", num);
+console.log("Str value", str);
+console.log("Boolean value", isBool);
+// javascrip-g jaahan urgutguud typescript (MS gargaj irsen)-g gargaj irj aldaagui bichih bolomjiig olgoson.
+function add(a, b) {
+    return a + b;
+}
+const y = add(100, 100);
+console.log("ADD", y);
+// zaaval toon utga butsaana gedriig bichne.
+const add2 = (a, b) => {
+    return a + b;
+};
+const myPrint = (text) => {
+    console.log("Hello", text);
+};
+// print n ugsun toog console.log ruu heveldeg function. herev VOID geed bichvel utga butsaagui functs.
+const s = add2(200, 500);
+myPrint("Welcome");
+// type imperence n anhnii utgaar n oilgono
+// bichij duusaad npx tsc index.ts-g JS ruu hurvuulne. Hurvuulsnii daraa node index.js-r ajilluulne
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
 console.log("hello Typescript");
 
-const num:number = 20;
-const str:string = "hello";
+const num: number = 20;
+const str: string = "hello";
 
-const isBool:boolean = true;
+const isBool: boolean = true;
 
 // typescript gedeg n turul buguud JS ruu durmiin daguu turluudiig n zaaj ugnu. Tegeed typescriptee javascript ruu hurvuulne. Ingesneer aldaag uridchilan typescripteer turul butiin durmiin daguu aldaagui bichne.
 
 // typescript => javascript ruu hurvuulne
 // --oOo--
 // typescript-g compider ashiglaj js ruu hurvuulne
-    // node js bwr zuvhun js-g ajilluuldag
+// node js bwr zuvhun js-g ajilluuldag
 
 console.log("Num value", num);
 console.log("Str value", str);
 console.log("Boolean value", isBool);
 
 // javascrip-g jaahan urgutguud typescript (MS gargaj irsen)-g gargaj irj aldaagui bichih bolomjiig olgoson.
-function add(a:number, b:number): number {
-    return a + b;
+function add(a: number, b: number): number {
+  return a + b;
 }
-const y:number = add(100, 100);
+const y: number = add(100, 100);
 console.log("ADD", y);
 
 // zaaval toon utga butsaana gedriig bichne.
-const add2 = (a:number, b:number): number => {
-    return a + b;
+const add2 = (a: number, b: number): number => {
+  return a + b;
 };
-const myPrint = (text:string): void => {
-    console.log("Hello", text);
+const myPrint = (text: string): void => {
+  console.log("Hello", text);
 };
 
 // print n ugsun toog console.log ruu heveldeg function. herev VOID geed bichvel utga butsaagui functs.
@@ -36,6 +36,18 @@ const myPrint = (text:string): void => {
 const s = add2(200, 500);
 myPrint("Welcome");
 
-    // type imperence n anhnii utgaar n oilgono
+// type imperence n anhnii utgaar n oilgono
 
-    // bichij duusaad npx tsc index.ts-g JS ruu hurvuulne. Hurvuulsnii daraa node index.js-r ajilluulne
+// bichij duusaad npx tsc index.ts-g JS ruu hurvuulne. Hurvuulsnii daraa node index.js-r ajilluulne
+
+// type huis: boolean HuisType er | en;
+
+interface IPerson {
+  name: string;
+  age: number;
+  //   huis: HuisType;
+}
+
+interface ITeacher extends IPerson {
+  lesson: string;
+}
